76ef9202160fd0cbf2ebdf9473bb87761249e5b3
7LucasLopes <ld981506390@gmail.com>
2026-09-12 16:49:39 -0300

    Organização dos arquivos

--- Exercicios/Ex_Beecrowd/beecrowd1019.py
+++ Exercicios/Ex_Beecrowd/beecrowd1019.py
@@ -5,7 +5,6 @@ resto = valor % 3600
 
 minuto = resto // 60
 
-
 segundo =  resto % 60
 
 print(f"{hora}:{minuto}:{segundo}")
---
+++ POO/poo.py
@@ -0,0 +1,24 @@
+#POO
+
+#Classe exemplo
+
+class Pessoa:
+    def __init__(self, nome, idade) -> None:
+        self.nome = nome
+        self.idade = idade
+
+    def saudacao(self):
+        return f"Olá, meu nome é {self.nome} e eu tenho {self.idade} anos"
+
+#Objetos
+
+pessoa1 = Pessoa("João", 50)
+#print(f"Nome: {pessoa1.nome}")
+#print("Idade:", pessoa1.idade)
+
+mensagem = pessoa1.saudacao()
+print(mensagem)
+
+pessoa2 = Pessoa(nome = "Lopes", idade = 15)
+mensagem = pessoa2.saudacao()
+print(mensagem)
